c885ec11b201cc20773bfea8e5aced8db8b6f482
angellobear <156106583+angellobear@users.noreply.github.com>
2026-07-18 16:13:02 -0500

    Change vehicle brand key separator to hyphen
    Updates toVehicleBrandKey() to use hyphens instead of underscores as separators. This improves URL readability and follows common web conventions for URL slugs.

--- lib/vehicle-brands-public.ts
+++ lib/vehicle-brands-public.ts
@@ -28,6 +28,6 @@ export function toVehicleBrandKey(name: string) {
     .toLowerCase()
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[^a-z0-9]+/g, '_')
-    .replace(/^_+|_+$/g, '')
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '')
 }
